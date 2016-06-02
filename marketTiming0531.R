a6725cdf1d07a94db55ec4495811339d7736a08a
Shuyue Fu <sfu11@illinois.edu>
2016-06-02 16:35:34 +0800

    Update marketTiming0531.R

--- marketTiming0531.R
+++ marketTiming0531.R
@@ -1,70 +1,101 @@
+findPosition = function(StartDate, EndDate, fundList){
+
+	len = 40
+	# select the suitable fund
+	year = as.POSIXlt(StartDate)$year+1900
+	if(year == 2016) {fund = fundList[[9]]} else {fund = fundList[[year-2007+1]]}
+
+	# extract time series of price
+	EquityNAV = w.wsd(fund[1],"trade_code,NAV_adj",StartDate - len + 1,EndDate,"PriceAdj=F")$Data[,c(1,3)]
+
+	for(i in 2:length(fund))
+	{
+		temp = w.wsd(fund[i],"trade_code,NAV_adj",StartDate - len + 1,EndDate,"PriceAdj=F")$Data
+		EquityNAV = cbind(EquityNAV,temp[,3])
+	}
+
+	# 计算净值变化率
+	Nav = EquityNAV[,-1][,-1] # 去掉日期
+	n = length(Nav[,1])
+	NavPCT = (Nav[-1,] - Nav[-n,])/Nav[-n,]
+	NavPCT = NavPCT*100
+	
+	# 回归变量
+	EstimationIndex = c("000300.SH","399905.SZ")
+	# 回归参数：回归长度 len, 每只基金回归（总数 n- len +1）次
+	
+	IndexPCT = w.wsd(EstimationIndex[1],"trade_code,pct_chg",StartDate - len + 1, EndDate,"PriceAdj=F")$Data[,c(1,3)]
+	for(i in 2:length(EstimationIndex))
+	{
+		temp = w.wsd(EstimationIndex[i],"trade_code,pct_chg",StartDate - len +1, EndDate,"PriceAdj=F")$Data
+		IndexPCT = cbind(IndexPCT,temp[,3])
+	}
+
+	IndexPCT = IndexPCT[-1,]
+	n = length(IndexPCT[,1]) # 总长度
+	date = IndexPCT[c(-(len-1):-1),1]
+	totalTime = n - len + 1
+	position = vector()
+
+	for (j in 0:(totalTime-1))
+	{
+		tempPosition = vector()
+		for(i in 1:length(NavPCT))
+		{
+			temp = summary(lm(NavPCT[i][(n-j-len+1):(n-j),1] ~ IndexPCT[(n-j-len+1):(n-j),2] + 
+																IndexPCT[(n-j-len+1):(n-j),3] ))$coefficients
+			coef = ifelse(temp[,4]>0.1,0,temp[,1])
+			tempPosition[i] = ifelse(sum(coef[2:3]) > 1,1,sum(coef[2:3]))
+		}
+
+		# 由于回归区间短，回归可能得到异常值，故只计算正常仓位平均值
+		position[j+1] = mean(tempPosition[tempPosition > 0.3])
+	}
+
+	# 计算平均仓位
+	rev(position)
+
+}
+
 library(WindR)
 w.start()
 
-StartDate = "2014-01-01"
-EndDate = "2016-05-30"
+source("findSignal0603.R")
+source("backTest.R")
 
-#提取股票型基金代码
-EquityFundName = w.wset('sectorconstituent','date=20160531;sectorid=2001010100000000;field=wind_code')
-EquityFundName = EquityFundName[2]$Data$wind_code
-count = 1
+# 创建基金样本列表
 
-# 提取净值数据
-EquityNAV = w.wsd(EquityFundName[1],"trade_code,NAV_adj",StartDate,EndDate,"PriceAdj=F")$Data[,c(1,3)]
-for(i in 2:length(EquityFundName))
+df = read.csv('fund.csv',stringsAsFactors = FALSE)
+threshold = 40
+
+df07 = df[df$date < (as.Date("2007-1-1") - threshold),1]
+df08 = df[df$date < (as.Date("2008-1-1") - threshold),1]
+df09 = df[df$date < (as.Date("2009-1-1") - threshold),1]
+df10 = df[df$date < (as.Date("2010-1-1") - threshold),1]
+df11 = df[df$date < (as.Date("2011-1-1") - threshold),1]
+df12 = df[df$date < (as.Date("2012-1-1") - threshold),1]
+df13 = df[df$date < (as.Date("2013-1-1") - threshold),1]
+df14 = df[df$date < (as.Date("2014-1-1") - threshold),1]
+df15 = df[df$date < (as.Date("2015-1-1") - threshold),1]
+
+fundList = list(df07, df08, df09, df10, df11, df12, df13, df14, df15)
+
+StartDate = as.Date("2007-01-01")
+EndDate = Sys.Date()
+
+position = findPosition(StartDate, StartDate + 364, fundList)
+tempStart = StartDate
+for(i in 1:8)
 {
-	temp = w.wsd(EquityFundName[i],"trade_code,fund_existingyear,NAV_adj",StartDate,EndDate,"PriceAdj=F")$Data
-	n = length(temp[,1])
-
-	# 仅计算存续期大于三年的基金
-	# count用于记录基金组合中共有几只基金
-	if(temp[n,3]>=3){
-	EquityNAV = cbind(EquityNAV,temp[,4])
-	count = count+1
-	}
+	tempStart = tempStart + 365
+	tempEnd = tempStart + 364
+	if(as.POSIXlt(tempStart)$year+1900 == 2008 | as.POSIXlt(tempStart)$year+1900 == 2012) {tempEnd = tempEnd + 1; tempStart = tempStart + 1}
+	position = c(position , findPosition(tempStart, tempEnd, fundList))
 }
 
-# 提取仓位估算指数组
-# 此程序使用的是最简单的沪深300，中证500和创业板指
-
-EstimationIndex = c("000300.SH","399905.SZ","399006.SZ")
-IndexPCT = w.wsd(EstimationIndex[1],"trade_code,pct_chg",StartDate,EndDate,"PriceAdj=F")$Data[,c(1,3)]
-for(i in 2:length(EstimationIndex))
-{
-	temp = w.wsd(EstimationIndex[i],"trade_code,pct_chg",StartDate,EndDate,"PriceAdj=F")$Data
-	IndexPCT = cbind(IndexPCT,temp[,3])
-}
-
-# 计算净值变化率
-Nav = EquityNAV[,-1][,-1] # 去掉日期
-n = length(Nav[,1])
-NavPCT = (Nav[-1,] - Nav[-n,])/Nav[-n,]
-NavPCT = NavPCT*100
-IndexPCT = IndexPCT[-1,]
-n = length(IndexPCT[,1]) # 总长度
-
-# 回归参数
-# 回归长度 len, 每只基金回归（总数 n- len +1）次
-len = 30
-date = IndexPCT[c(-(len-1):-1),1]
-totalTime = n - len + 1
-position = vector()
-
-for (j in 0:(totalTime-1))
-{
-	tempPosition = vector()
-	for(i in 1:length(NavPCT))
-	{
-		temp = summary(lm(NavPCT[i][(n-j-len+1):(n-j),1] ~ IndexPCT[(n-j-len+1):(n-j),2] + IndexPCT[(n-j-len+1):(n-j),3] 
-														   + IndexPCT[(n-j-len+1):(n-j),4]))$coefficients
-		coef = ifelse(temp[,4]>0.1,0,temp[,1])
-		tempPosition[i] = ifelse(sum(coef[2:4]) > 1,1,sum(coef[2:4]))
-	}
-
-	# 由于回归区间短，回归可能得到异常值，故只计算正常仓位平均值
-	position[j+1] = mean(tempPosition[tempPosition > 0.5])
-}
-
+tempStart = as.Date("2016-01-01")
+EndDate = Sys.Date()
+position = c(position , findPosition(tempStart, EndDate, fundList[[9]]))
 
 # 计算平均仓位
 MeanPosition = position
@@ -72,21 +103,24 @@ MeanPosition = position
 # 计算布林线上下轨
 n = length(MeanPosition)
 K = 5
-totalTime = n - K + 1
+totalTime = n - K 
 upper = lower = vector()
-for(i in 0:(totalTime-1))
+for(i in 1:totalTime)
 {
 	# 前 K 日平均加减标准差
-	avg = mean(MeanPosition[(n-i-K+1):(n-i)])
-	stdev = sd(MeanPosition[(n-i-K+1):(n-i)])
-	upper[i+1] = avg + K * stdev
-	lower[i+1] = avg - K * stdev
+	avg = mean(MeanPosition[i:(i+K-1)])
+	stdev = sd(MeanPosition[i:(i+K-1)])
+	upper[i] = avg + K * stdev
+	lower[i] = avg - K * stdev
 }
+
 
 
 # 寻找信号, 低于下轨，signal为1，做多，反之做空
 # 发现信号后都默认在第二天开盘执行
-df = data.frame(date[(length(date)-n+K):length(date)],MeanPosition[K:n],lower,upper)
+HS300 = w.wsd("000300.SH","close",StartDate,EndDate,"PriceAdj=F")$Data
+date = HS300[,1]
+df = data.frame(date[(length(date)-n+K+1):length(date)],MeanPosition[(K+1):n],lower,upper)
 colnames(df) = c('date','EstimatedPosition','lowerBound','upperBound')
 
 signal = vector()
@@ -108,7 +142,7 @@ for(i in 2:dim(df)[1])
 }
 
 
-date = date[(length(date)-n+K):length(date)]
+HS300 = HS300[(length(date)-n+K+1):length(date),]
 n = length(signal)
 
 
@@ -116,94 +150,22 @@ n = length(signal)
 library('ggplot2')
 p = ggplot() + geom_line(data = df, aes(x = date, y = EstimatedPosition),size = 1, colour = 'tomato1') + 
 				xlab('日期') + ylab('仓位')
-q = p + geom_line(data = df, aes(x = date, y = upper), size = 1, colour = 'steelblue2') + 
-		geom_line(data = df, aes(x = date, y = lower),size = 1, colour = 'steelblue2')
+q = p + geom_line(data = df, aes(x = date, y = upper), size = 1, colour = 'grey') + 
+		geom_line(data = df, aes(x = date, y = lower),size = 1, colour = 'grey')
 
 q
+length(which(signal>0))
 
+length(which(signal<0))
 
 # 策略回测
-CYB = w.wsd("399905.SZ","trade_code,close",as.character(df$date[1]),as.character(df$date[n]),"PriceAdj=F")$Data
-HS300 = w.wsd("000300.SH","trade_code,close",as.character(df$date[1]),as.character(df$date[n]),"PriceAdj=F")$Data
-ZZ500 = w.wsd("399905.SZ","trade_code,close",as.character(df$date[1]),as.character(df$date[n]),"PriceAdj=F")$Data
-index = data.frame(CYB$CLOSE,HS300$CLOSE,ZZ500$CLOSE)
-colnames(index) = c('CYB','HS300','ZZ500')
+plotNAV = backTest(signal, HS300)
+evaluate1(plotNAV)
+evaluate2(plotNAV)
 
-profit = vector()
-record = vector()
-action = vector()
-myNAV = vector()
-commission = vector()
-mytime = vector()
-myNAV[1] = index[1,2]
-mytime[1] = 0
-j = 1
-jj = 1
-flag = 0
-
-for(i in 2:n)
-{
-	if(flag == -1)
-	{	
-		profit[jj] = index[i,2] - index[i-1,2]
-		myNAV[jj+1] = myNAV[jj] + profit[jj]
-		mytime[jj+1] = as.character(date[i])
-		jj = jj + 1
-	}
-
-	else
-	{
-		if(flag == 1)
-		{
-			profit[jj] = index[i-1,2] - index[i,2]
-			myNAV[jj+1] = myNAV[jj] + profit[jj]
-			mytime[jj+1] = as.character(date[i])
-			jj = jj + 1
-		}
-	}
-
-	if(signal[i] < signal[i-1])
-	{
-		# 由做多变为做空，sell
-		action[j] = 'start sell'
-		record[j] = as.character(date[i+1])
-		myNAV[jj] = myNAV[jj] - 0.0002*2*index[i+1,2]
-		j = j + 1
-		flag = -1
-	}
-	
-	else
-	{
-		if(signal[i] > signal[i-1])
-		{
-			# 由做空变为做多
-			action[j] = 'start buy'
-			record[j] = as.character(date[i+1])
-			myNAV[jj] = myNAV[jj] - 0.0002*2*index[i+1,2]
-			j = j + 1
-			flag = 1
-		}
-	}
-
-}
-mytime[1] = record[1]
-record = cbind(record,action)
-myNAV = myNAV/myNAV[1]
-
-plotNAV = data.frame(date[length(date)-531,length(date)],myNAV)
-colnames(plotNAV) = c(date,NAV)
-p = ggplot() + geom_line(data = plotNAV, aes(x = date, y = NAV),size = 2, colour = 'tomato1')
-
-
-
-library(tseries)
-maxDrawDown = maxdrawdown(myNAV)
-
-
-
-
-
-
-
+# plot
+p = ggplot() + geom_line(data = plotNAV, aes(x = date, y = myNAV),size = 1, colour = 'tomato1')
+p = p + geom_line(data = plotNAV, aes(x = date, y = index/index[1]),size = 1, colour = 'grey') 
+p
 
 
